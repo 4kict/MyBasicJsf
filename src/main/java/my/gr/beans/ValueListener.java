35ff887b7439ad286c9efa88af30a7466de5e36c
SBT-Kiladze-GS <GSKiladze.SBT@sberbank.ru>
2017-12-29 14:44:00 +0300

    Initial commit

---
+++ src/main/java/my/gr/beans/ValueListener.java
@@ -0,0 +1,13 @@
+package my.gr.beans;
+
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ValueChangeListener;
+
+public class ValueListener  implements ValueChangeListener {
+
+    public void processValueChange(ValueChangeEvent event) throws AbortProcessingException {
+        System.out.println("ЧекМи");
+    }
+
+}
